ecd0ca01766fe7f88ffaf700c3f5741ed345b593
mogamoga <mogamoga.g1thu6@gmail.com>
2020-08-16 00:28:03 +0900

    test追加

--- test/test.js
+++ test/test.js
@@ -6,8 +6,8 @@ module("mancalaInitのテスト", {
 });
 
 test("Holesの要素数", function () {
-    strictEqual(playerHoles.length, 6, "playerHoles");
-    strictEqual(cpuHoles.length, 6, "cpuHoles");
+    strictEqual(playerHoles.length, 6, "playerHoles.length");
+    strictEqual(cpuHoles.length, 6, "cpuHoles.length");
 });
 
 test("Holesの要素", function() {
@@ -32,12 +32,86 @@ test("Holesの要素", function() {
     strictEqual(cpuStore.isStore, true, "cpuStore isStore");
 });
 
-module("copyHolesのテスト", {
+module("stoneMoveのテスト", {
     setup: function() {
         mancalaInit();
     }
 });
 
-test("要素のコピーテスト", function() {
+test("Holesの要素", function() {
+    stoneMove(playerHoles[1]);
+    stoneMove(cpuHoles[3]);
+    stoneMove(playerHoles[4]);
+    stoneMove(cpuHoles[0]);
+    stoneMove(playerHoles[5]);
 
+    let hole = playerHoles[0];
+    strictEqual(hole.stoneCount, 5, "playerHoles[0].stoneCount");
+    hole = hole.nextHole;
+    strictEqual(hole.stoneCount, 0, "playerHoles[1].stoneCount");
+    hole = hole.nextHole;
+    strictEqual(hole.stoneCount, 5, "playerHoles[2].stoneCount");
+    hole = hole.nextHole;
+    strictEqual(hole.stoneCount, 5, "playerHoles[3].stoneCount");
+    hole = hole.nextHole;
+    strictEqual(hole.stoneCount, 0, "playerHoles[4].stoneCount");
+    hole = hole.nextHole;
+    strictEqual(hole.stoneCount, 0, "playerHoles[5].stoneCount");
+    hole = hole.nextHole;
+    strictEqual(hole.stoneCount, 2, "playerStore.stoneCount");
+    hole = hole.nextHole;
+    strictEqual(hole.stoneCount, 1, "cpuHoles[0].stoneCount");
+    hole = hole.nextHole;
+    strictEqual(hole.stoneCount, 7, "cpuHoles[1].stoneCount");
+    hole = hole.nextHole;
+    strictEqual(hole.stoneCount, 7, "cpuHoles[2].stoneCount");
+    hole = hole.nextHole;
+    strictEqual(hole.stoneCount, 2, "cpuHoles[3].stoneCount");
+    hole = hole.nextHole;
+    strictEqual(hole.stoneCount, 7, "cpuHoles[4].stoneCount");
+    hole = hole.nextHole;
+    strictEqual(hole.stoneCount, 6, "cpuHoles[5].stoneCount");
+    hole = hole.nextHole;
+    strictEqual(hole.stoneCount, 1, "cpuStore.stoneCount");
 });
+
+module("copyHolesのテスト", {
+    setup: function() {
+        mancalaInit();
+        stoneMove(playerHoles[5]);
+        stoneMove(cpuHoles[2]);
+        stoneMove(playerHoles[3]);
+        stoneMove(cpuHoles[0]);
+
+    }
+});
+
+test("要素のコピーテスト:要素数", function() {
+    const holesList = copyHoles(playerHoles, cpuHoles);
+    const cpyPlayerHoles = holesList.playerHoles;
+    const cpyCpuHoles = holesList.cpuHoles;
+    
+    strictEqual(cpyPlayerHoles.length, 6, "cpyPlayerHoles.length");
+    strictEqual(cpyCpuHoles.length, 6, "cpyCpuHoles.length");
+});
+
+test("要素のコピーテスト:要素", function() {
+    const holesList = copyHoles(playerHoles, cpuHoles);
+    const cpyPlayerHoles = holesList.playerHoles;
+    const cpyCpuHoles = holesList.cpuHoles;
+    
+    for (let i = 0; i < cpyPlayerHoles.length; i++) {
+        let originHole = playerHoles[i];
+        let hole = cpyPlayerHoles[i];
+        strictEqual(hole.stoneCount, originHole.stoneCount, "cpyPlayerHoles stoneCount");
+        strictEqual(hole.isStore, false, "cpyPlayerHoles isStore");
+    }
+
+    for (let i = 0; i < cpyCpuHoles.length; i++) {
+        let originHole = cpuHoles[i];
+        let hole = cpyCpuHoles[i];
+        strictEqual(hole.stoneCount, originHole.stoneCount, "cpuHoles stoneCount");
+        strictEqual(hole.isStore, false, "cpuHoles isStore");
+    }
+});
+
